0d997fda6f1a0d2f2880526007b5516c316a26cc
mrt-stl <martin@stadtteilliebe.de>
2022-01-06 10:30:59 +0100

    style select input (not done)

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants"
+import Icon from "../Icon"
+
+// NOT RIGHT
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const [selectWidth, setSelectWidth] = useState(40)
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    useEffect(() => {
+        const displayedValue = value
+        setSelectWidth(20 + displayedValue.length * 30)
+    }, [value])
 
-export default Select;
+    return (
+        <>
+            {label ? <SelectLabel htmlFor={label}>{label}</SelectLabel> : null}
+            <SelectWrapper>
+                <SelectElement value={value} onChange={onChange} name={label} width={`${selectWidth}px`}>
+                    {children}
+                </SelectElement>
+                <SelectIcon id="chevron-down" size="24" strokeWidth={2} />
+            </SelectWrapper>
+        </>
+    )
+}
+
+const SelectWrapper = styled.div`
+    display: inline-block;
+    position: relative;
+`
+
+const SelectLabel = styled.label`
+    display: block;
+`
+
+const SelectIcon = styled(Icon)`
+    position: absolute;
+    top: 11px;
+    right: 14px;
+
+    svg {
+        stroke: ${COLORS.gray700};
+    }
+`
+
+const SelectElement = styled.select`
+    appearance: none;
+    width: ${(props) => props.width};
+    padding: 12px 52px 12px 16px;
+    height: 43px;
+    font-size: 16px;
+    color: ${COLORS.gray700};
+    background-color: ${COLORS.transparentGray15};
+    border-radius: 8px;
+    border: 0px;
+
+    :hover {
+        color: ${COLORS.black};
+
+        ${SelectIcon} {
+            svg {
+                stroke: ${COLORS.black};
+            }
+        }
+    }
+`
+
+export default Select
